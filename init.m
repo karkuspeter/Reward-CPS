9363d278deb800548ea56d91dde4f8f7b07ef03d
karkuspeter <karkuspeter@gmail.com>
2016-08-04 19:07:00 +0800

    extending init file, add acq function for ACES

---
+++ init.m
@@ -0,0 +1,6 @@
+run ../../gpml-matlab-v3.6-2015-07-07/startup.m;
+addpath ../../SPGP_dist;
+addpath ../../altmany-export_fig-b894ce6;
+addpath ../../TPROD;
+addpath ../../EmGm;
+addpath ../../EntropySearch;
