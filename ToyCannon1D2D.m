2e8ebfaf3cc8d55fda223196e3662b362b6892b3
Peter Karkus <karkus@Peters-MacBook-Pro.local>
2016-08-20 14:46:32 +0800

    caching optimal values

--- ToyCannon1D2D.m
+++ ToyCannon1D2D.m
@@ -30,11 +30,10 @@ classdef ToyCannon1D2D < ProblemInterface
             theta_vec = [];
             r_vec = [];
             [x1, x2] = ndgrid(linspace(obj.theta_bounds(1,1),obj.theta_bounds(1,2), datapoints), ...
-                linspace(obj.theta_bounds(2,1),obj.theta_bounds(2,2), datapoints));
-            
-            for context = linspace(obj.st_bounds(1,1), obj.st_bounds(1,2), contextpoints)
-                y = arrayfun(@(t1, t2)(obj.sim_eval_func(context, [t1 t2])), x1, x2);
-                
+                linspace(obj.theta_bounds(2,1),obj.theta_bounds(2,2), datapoints));           
+             
+            for context_id = 1:contextpoints
+                y = obj.get_cached_grid(context_id, datapoints, contextpoints);
                 [r_opt, ind] = max(y(:)); %will return 1d indexing
                 theta_opt = [x1(ind), x2(ind)];
                 
@@ -44,16 +43,28 @@ classdef ToyCannon1D2D < ProblemInterface
             
         end
         
-        function y = get_cached_grid(obj, context_id)
+        function y = get_cached_grid(obj, context_id, datapoints, contextpoints)
             
 
-            if ~obj.grid_cache
+            if isempty(obj.grid_cache)
                 if exist('ToyCannon1D2Dcache.mat', 'file') == 2
-                    obj.grid_cache = load('ToyCannon1D2Dcache.mat', 'grid_cache');
+                    load('ToyCannon1D2Dcache.mat', 'grid_cache');
+                    obj.grid_cache = grid_cache;
+                else
+                    obj.grid_cache = [];
+                    [x1, x2] = ndgrid(linspace(obj.theta_bounds(1,1),obj.theta_bounds(1,2), datapoints), ...
+                        linspace(obj.theta_bounds(2,1),obj.theta_bounds(2,2), datapoints));
+                    
+                    for context = linspace(obj.st_bounds(1,1), obj.st_bounds(1,2), contextpoints)
+                        y = arrayfun(@(t1, t2)(obj.sim_eval_func(context, [t1 t2])), x1, x2);
+                        obj.grid_cache(:,:,end+1) = y;
+                    end
+                    
+                    grid_cache = obj.grid_cache;
+                    save('ToyCannon1D2Dcache.mat', 'grid_cache');
                 end
                 
                 
-            
             end
             y = obj.grid_cache(:,:,context_id);
         end
