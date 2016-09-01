b5cf465c916a7a5ace35c3b5c793557dbc8ac188
Peter Karkus <karkus@Peters-MacBook-Pro.local>
2016-09-01 19:32:08 +0800

    one more file missing

---
+++ ACES/LossFunction.m
@@ -0,0 +1,16 @@
+function [ dHp ] = LossFunction( GP, logP, zb, lmb, x, params)
+%U returns relative entropy
+
+lPred = PredPmin(GP, zb, x, params);
+
+% this is not ACES!
+
+H   = - sum(exp(logP) .* (logP + lmb));           % current Entropy 
+mlPred = mean(lPred,2);
+dHp = -sum(exp(mlPred) .* bsxfun(@plus,mlPred,lmb),1) - H; % @minus? If you change it, change it above in H, too!
+
+%dHp = mean(dHp_all);
+%dHp = mean(lPred, 2);
+
+end
+
