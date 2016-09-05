7ca2e001715090085aeb850a60150e6c08e865d4
karkuspeter <karkuspeter@gmail.com>
2016-09-06 00:14:45 +0800

    improved process params function

--- ProcessParams.m
+++ ProcessParams.m
@@ -2,12 +2,20 @@ function [ params ] = ProcessParams( defaults,  input_params)
 %overright params that are set in input_params
 
     params = defaults;
-    fields = fieldnames(params);
+    fields = fieldnames(input_params);
     for i=1:numel(fields)
-        if isfield(input_params, fields{i})
-            params.(fields{i}) = input_params.(fields{i});
+        if ~isfield(defaults, fields{i})
+            fprintf('Warning: %s is not a parameter\n',fields{i});
         end
+        params.(fields{i}) = input_params.(fields{i});
+        
     end
+%     fields = fieldnames(params);
+%     for i=1:numel(fields)
+%         if isfield(input_params, fields{i})
+%             params.(fields{i}) = input_params.(fields{i});
+%         end
+%     end
 
 end
 
