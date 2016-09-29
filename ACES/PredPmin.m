c3acdb3b0c6d8d2d1007b95a9265f1f243f0bb24
karkuspeter <karkuspeter@gmail.com>
2016-09-29 16:10:07 +0800

    use robust chol in PredPmin (it failed once)

--- ACES/PredPmin.m
+++ ACES/PredPmin.m
@@ -6,7 +6,7 @@ function pmin_pred = PredPmin(GP, zb, xnew, params)
 pmin_pred = zeros(size(zb,1),params.Ny);
 %[m my_s2] = gp(GP.hyp, [], [], GP.covfunc, GP.likfunc, GP.x, GP.y, xnew);
 [m, V] = GP_moments(GP, xnew);
-cholV = chol(V);
+cholV = robustchol(V);
 
 for i=1:params.Ny
     ynew = m + randn(size(m))*cholV; % sample from bivariate norm distr
@@ -14,7 +14,7 @@ for i=1:params.Ny
     GPnew.x = [GPnew.x; xnew];
     GPnew.y = [GPnew.y; ynew];        
     GPnew.K              = k_matrix(GPnew,GPnew.x) + diag(GP_noise_var(GPnew,GPnew.y));
-    GPnew.cK             = chol(GPnew.K);
+    GPnew.cK             = robustchol(GPnew.K);
     
     pmin_pred(:,i) = EstPmin(GPnew, zb, params.S, randn(size(zb,1),params.S));
     %TODO random numbers should be generated once to decrease noise
