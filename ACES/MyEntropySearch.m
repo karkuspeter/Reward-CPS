2729d0f5f05b82df6490d8f50c632961931d2c10
karkuspeter <karkuspeter@gmail.com>
2016-09-30 20:18:21 +0800

    move helperfunction to separate file

--- ACES/MyEntropySearch.m
+++ ACES/MyEntropySearch.m
@@ -919,18 +919,3 @@ for iiii=1:length(xmin)
 end
 [evalgrid{:}] = ndgrid(gridvect{:});
 end
-
-function evalvect = evalvectfun(xmin, xmax, Neval)
-if isempty(xmin)
-    evalvect = zeros(1,0);
-    return
-end
-evalgrid = cell(1, length(xmin));
-gridvect = cell(1, length(xmin));
-for iiii=1:length(xmin)
-    gridvect{iiii} = linspace(xmin(iiii), xmax(iiii), Neval(iiii))';
-end
-[evalgrid{:}] = ndgrid(gridvect{:});
-
-evalvect = [evalgrid{:}(:)];
-end
