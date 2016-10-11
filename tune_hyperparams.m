865e2b49bbdd9982e81609c59971bfa5ddf5675f
karkuspeter <karkuspeter@gmail.com>
2016-10-11 15:25:02 +0800

    minor change to plt easier tuning results

--- tune_hyperparams.m
+++ tune_hyperparams.m
@@ -118,7 +118,7 @@ res_list = hp_list;
 %save
 save(strcat('results/hyper-', datestr(now,'dd-mm-yyyy-HH-MM'), '.mat'));
 
-% show specific result
+%% show specific result
 figure
 indices = 1:numel(hp_list); %[4 5 9 10];%1:numel(hp_list);
 %indices = 1:4;
@@ -130,11 +130,12 @@ for ind = indices
     params = h_params{ind};
     labels = [labels, {num2str(ind)}];
     params
-    show_func()
+    ShowRepeatResults()
     plots = [plots, {h}];
 end
 legend([plots{:}], labels);
 
+%%
 h_cumm_mean = [];
 h_cumm_std = [];
 for ind = 1:length(hp_list)
