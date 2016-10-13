ce942040d139963543ddbd39487061d2030bfada
karkuspeter <karkuspeter@gmail.com>
2016-10-13 17:04:33 +0800

    compute optimal params for 3D cannon

--- ToyCannonBase3D.m
+++ ToyCannonBase3D.m
@@ -33,10 +33,44 @@ classdef ToyCannonBase3D < ToyCannonBase
         end
         
         function [r_opt, r_worse] = get_optimal_r(obj, Neval)
-            r_opt = 0;
+            dim = size(obj.st_bounds,1)+size(obj.se_bounds,1);
+            evalgrid = evalgridfun(...
+                [obj.st_bounds(:,1); obj.se_bounds(:,1)]', ...
+                [obj.st_bounds(:,2); obj.se_bounds(:,2)]', ...
+                Neval(1:dim));
+            r_opt = obj.get_optimal_for_s(evalgrid{:}, Neval(dim+1:end));
             r_worse = 2;
         end
+        function [r_opt, r_worse] = get_optimal_for_s(obj, s1, s2, Neval)
+            ang_h = atan2(s2, s1); 
+            ang_h(ang_h<0) = ang_h(ang_h<0) + 2*pi;
+            evalgrid = cell(1, 3);
+            gridvect = cell(1, 3);
+            gridvect{1} = linspace(obj.theta_bounds(1,1), obj.theta_bounds(1,2), Neval(1))';
+            gridvect{2} = ang_h(:);
+            gridvect{3} = linspace(obj.theta_bounds(3,1), obj.theta_bounds(3,2), Neval(3))';
+            [evalgrid{:}] = ndgrid(gridvect{:});
 
+            val_full = zeros(size(evalgrid{1}));
+            for i = 1:size(evalgrid{1},1)
+                for j = 1:size(evalgrid{1},2)
+                    for k = 1:size(evalgrid{1},3)
+                        val_full(i,j,k) = obj.sim_eval_func([s1(j), s2(j), evalgrid{1}(i,j,k), evalgrid{2}(i,j,k), evalgrid{3}(i,j,k)]);
+                    end
+                end
+            end
+            r_opt = val_full;
+            r_opt = min(r_opt, [], 1);
+            r_opt = min(r_opt, [], 3);
+            r_opt = reshape(r_opt, size(ang_h));
+  
+%             f=@(x)obj.sim_eval_func([s, x']);
+%             [minval1,xatmin1,hist] = Direct(struct('f', f), obj.theta_bounds, struct('showits', 1, 'maxevals', 200));
+%             [xatmin2, minval2] = fmincon(f, xatmin1, [], [], [], [], obj.theta_bounds(:,1), obj.theta_bounds(:,2), [], optimoptions('fmincon', 'Display', 'none'));
+            
+            r_worse = 2;            
+        end
+        
     end
     
 end
